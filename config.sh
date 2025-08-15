cde1493c323db30a140b4c51606450767efe89e8
root <danijanuar201@gmail.com>
2025-08-15 17:34:29 +0700

    TikTok Login Kit (Flask) initial

--- config.sh
+++
@@ -1,5 +0,0 @@
-export FLASK_SECRET='ganti-ini'
-export TIKTOK_CLIENT_KEY='xxx'
-export TIKTOK_CLIENT_SECRET='yyy'
-export TIKTOK_REDIRECT_URI='https://yourapp.com/auth/tiktok/callback'
-python app.py
